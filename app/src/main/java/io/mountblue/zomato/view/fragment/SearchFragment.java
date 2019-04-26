20205dc415c64cffcdd2737d29e3d9410a9dcadd
saurav kumar <saurav.kumar@mountblue.io>
2019-04-26 12:39:28 +0530

    Increase delay time in search (from 300 to 500)

--- app/src/main/java/io/mountblue/zomato/view/fragment/SearchFragment.java
+++ app/src/main/java/io/mountblue/zomato/view/fragment/SearchFragment.java
@@ -18,7 +18,11 @@ import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+
+import com.jakewharton.rxbinding2.widget.RxTextView;
+import com.jakewharton.rxbinding2.widget.TextViewTextChangeEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,9 +39,14 @@ import io.mountblue.zomato.module.Restaurant;
 import io.mountblue.zomato.module.RestaurantResponse;
 import io.reactivex.Observable;
 import io.reactivex.Observer;
+import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.PublishSubject;
 
 public class SearchFragment extends Fragment {
     private static final String TAG = "SearchFragment";
@@ -50,6 +59,13 @@ public class SearchFragment extends Fragment {
     ProgressBar searchProgressBar;
     @BindView(R.id.search_close)
     ImageView searchClose;
+    @BindView(R.id.no_result)
+    LinearLayout noResultMessage;
+
+    private CompositeDisposable disposable = new CompositeDisposable();
+    private PublishSubject<String> publishSubject = PublishSubject.create();
+    private RestaurantService apiService;
+
 
 
     List<Restaurant> restaurantList;
@@ -73,69 +89,107 @@ public class SearchFragment extends Fragment {
         restaurantRecyclerView.setLayoutManager(layoutManager);
         restaurantRecyclerView.smoothScrollToPosition(1);
 
-        typedText.addTextChangedListener(new TextWatcher() {
 
-            public void afterTextChanged(Editable s) {
-            }
-
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-                searchClose.setVisibility(View.GONE);
-                if (count > 0) {
-                    searchProgressBar.setVisibility(View.VISIBLE);
-                    Observable<RestaurantResponse> apiService = ApiClient.getRetrofitInstance().create(RestaurantService.class)
-                            .getSearch(1, latitude, longitude, s)
-                            .subscribeOn(Schedulers.io())
-                            .observeOn(AndroidSchedulers.mainThread());
-                    apiService.subscribe(new Observer<RestaurantResponse>() {
-                        @Override
-                        public void onSubscribe(Disposable d) {
-
-                        }
-
-                        @Override
-                        public void onNext(RestaurantResponse restaurantResponse) {
-                            Log.e(TAG, "onNext: " + restaurantResponse.getRestaurants().size());
-                            restaurantList.clear();
-                            restaurantList=new ArrayList<>();
-                            restaurantList.addAll(restaurantResponse.getRestaurants());
-                            SearchAdapter  searchAdapter = new SearchAdapter(getContext());
-                            searchAdapter.setRestaurantList(restaurantList);
-                            restaurantRecyclerView.setAdapter(searchAdapter);
-                            searchAdapter.notifyDataSetChanged();
-                            searchProgressBar.setVisibility(View.GONE);
-                            searchClose.setVisibility(View.VISIBLE);
-                        }
-
-                        @Override
-                        public void onError(Throwable e) {
-                            Log.e(TAG, "onError: " + e.getStackTrace());
-                        }
-
-                        @Override
-                        public void onComplete() {
-
-                        }
-                    });
-
-                }
-
-            }
-        });
         typedText.requestFocus();
         searchClose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 typedText.setText("");
                 typedText.clearFocus();
+                searchClose.setVisibility(View.GONE);
                 InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Service.INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
             }
         });
+
+        apiService = ApiClient.getRetrofitInstance().create(RestaurantService.class);
+
+        DisposableObserver<RestaurantResponse> observer = getSearchObserver();
+
+        disposable.add(publishSubject.debounce(300, TimeUnit.MICROSECONDS)
+                .distinctUntilChanged()
+                .switchMapSingle(new Function<String, Single<RestaurantResponse>>() {
+                    @Override
+                    public Single<RestaurantResponse> apply(String s) throws Exception {
+                        return apiService.getData(1, latitude, longitude, s)
+                                .subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.mainThread());
+                    }
+                })
+                .subscribeWith(observer));
+
+
+        disposable.add(RxTextView.textChangeEvents(typedText)
+                .skipInitialValue()
+                .debounce(500, TimeUnit.MILLISECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(searchContactsTextWatcher()));
+
+        disposable.add(observer);
+
+        publishSubject.onNext("");
+
+
         return view;
     }
+    private DisposableObserver<RestaurantResponse> getSearchObserver() {
+        return new DisposableObserver<RestaurantResponse>() {
+            @Override
+            public void onNext(RestaurantResponse restaurants) {
+                Log.e(TAG, "onNext: " + restaurants.getRestaurants().size());
+                if (restaurants.getRestaurants().size() > 0){
+                    noResultMessage.setVisibility(View.GONE);
+                }
+                else {
+                    noResultMessage.setVisibility(View.VISIBLE);
+                }
+                restaurantList.clear();
+                restaurantList = new ArrayList<>();
+                restaurantList.addAll(restaurants.getRestaurants());
+                SearchAdapter searchAdapter = new SearchAdapter(getContext());
+                searchAdapter.setRestaurantList(restaurantList);
+                restaurantRecyclerView.setAdapter(searchAdapter);
+                searchAdapter.notifyDataSetChanged();
+                searchProgressBar.setVisibility(View.GONE);
+                if (typedText.hasFocus()){
+                    searchClose.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e.getStackTrace());
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+    }
+
+    private DisposableObserver<TextViewTextChangeEvent> searchContactsTextWatcher() {
+        return new DisposableObserver<TextViewTextChangeEvent>() {
+            @Override
+            public void onNext(TextViewTextChangeEvent textViewTextChangeEvent) {
+                Log.d(TAG, "Search query: " + textViewTextChangeEvent.text());
+                publishSubject.onNext(textViewTextChangeEvent.text().toString());
+                if (typedText.hasFocus()){
+                    searchProgressBar.setVisibility(View.VISIBLE);
+                }
+                searchClose.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+            }
+        };
+    }
+
 }
